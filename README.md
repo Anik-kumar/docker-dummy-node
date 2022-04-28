09b8b9d723f66e90f1b8978bc505816a00313b84
Anik-kumar <anik.kumar.sarker@gmail.com>
2022-04-28 16:03:05 +0600

    updated server to connect mongodb

--- README.md
+++ README.md
@@ -1,8 +1,92 @@
-># A simple project on Docker & Node
+# A simple project on Docker & Node
+A simple ui backend application using `JavaScript`, `Node`, `Express` to integrate with docker container of `MongoDB` database.
 
 <br>
 
-### Docker
-### Node
-### Express
+### Docker | Node | Express | MongoDB
 
+          
+    node --> MongoDB(docker) <--> MongoExpress(docker) 
+
+
+<hr>
+<br>
+
+## Basic Docker Commands
+> docker pull 
+
+> docker run
+
+> docker start
+
+> docker stop
+
+> docker ps
+
+> docker exec -it 
+
+> docker logs
+
+<br>
+<hr>
+<br>
+
+## Developing with Containers
+### pull docker image
+1. `docker pull mongo`
+2. `docker pull mongo-express`
+
+> next step is to run mongo and mongo-express containers  in order to make mongodb database available for application and also connect
+
+### create docker network
+`docker network create mongo-network`
+
+> now have to run mongo container inside mongo-express container in docker network
+
+### run containers
+```
+docker run -d \
+-p 27017:27017 \
+-e MONGO_INITDB_ROOT_USERNAME=admin \
+-e MONGO_INITDB_ROOT_PASSWORD=password \
+--name mongodb-container \
+--net mongo-network \
+mongo
+```
+
+```
+docker run -d \
+-p 8081:8081 \
+-e ME_CONFIG_MONGODB_ADMINUSERNAME=admin \
+-e ME_CONFIG_MONGODB_ADMINPASSWORD=password \
+-e ME_CONFIG_MONGODB_SERVER=mongodb-container \
+--name mongo-express-container \
+--net mongo-network \
+mongo-express
+```
+
+> run `docker ps` to check if its running.
+
+> Go to browser localhost:8081 and create a database "user-account" and create collection "users"
+### connect node server with mongo express
+> go to local nodejs project and start the project.
+
+    node server.js
+
+> Go to localhost:3000
+
+
+<br>
+<hr>
+
+[-p]: port flag.
+
+[-d]: detachable mode flag.
+
+[-e]: environmental variable flag.
+
+[--name]: name of the running container.
+
+[--net]: name of the network.
+
+[--rm]: auto removes the container when stops.
--- server.js
+++ server.js
@@ -2,17 +2,63 @@
 
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = 3000;
+const mongoLocalUrl = "mongodb://admin:password@localhost:27017";
+const mongoDockerUrl = "mongodb://admin:password@mongodb-container";
+const mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+const database = "user-account";
 
-// App
-const app = express();
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.get('/get-profile', (req, res) => {
+
+  MongoClient.connect(mongoLocalUrl, mongoClientOptions, (err, client) => {
+    if(err) throw err;
+    // console.log("COnnected to database");
+    let db = client.db(database);
+    let query = { userId: 1 };
+    db.collection('users').findOne(query, (err, result) => {
+      if(err) throw err;
+
+      client.close();
+      // console.log("Disconnected from database");
+      res.send(result);
+    });
+  });
+});
+
+app.post('/update-profile', (req, res) => {
+  const userObj = req.body;
+  // console.log("user obj ", userObj);
+  // console.log('connecting to db...');
+  userObj['userId'] = 1;
+
+  MongoClient.connect(mongoLocalUrl, mongoClientOptions, (err, client) => {
+    if(err) throw err;
+    // console.log("COnnected to database");
+    let db = client.db(database);
+    let query = { userId: 1 };
+    let newValue = { $set: userObj };
+    db.collection('users').updateOne(query, newValue, {upsert: true}, (err, result) => {
+      if(err) throw err;
+      // console.log("Data updated");
+      client.close();
+      // console.log("Disconnected from database");
+    });
+  });
+  res.send(userObj);
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on ${PORT}`);
+});
